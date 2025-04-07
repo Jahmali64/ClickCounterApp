479161b41469c7ea9baf891aa7c4a9b01676cbc4
Jahmali Mcknight <jahmali@rampinteractive.onmicrosoft.com>
2025-04-07 17:29:44 -0600

    Add material ui theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import {CssBaseline} from '@mui/material';
+import {ThemeProvider, createTheme} from '@mui/material/styles';
 import './index.css'
 import App from './App.tsx'
 
+const theme = createTheme();
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <App/>
+        </ThemeProvider>
+    </StrictMode>
 )
